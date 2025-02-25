84e732cfbcd416501d998075b93b668859f83543
Chris <chris@idlelion.net>
2025-02-24 18:06:16 -0600

    Add user columns, actions, and views

---
+++ db/migrate/20250224224123_add_user_fields.rb
@@ -0,0 +1,7 @@
+class AddUserFields < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :handle, :string, null: false
+    add_column :users, :active, :boolean
+    add_index :users, :handle, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_24_194054) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_224123) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -25,7 +25,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_24_194054) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "handle", null: false
+    t.boolean "active"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["handle"], name: "index_users_on_handle", unique: true
   end
 
   add_foreign_key "sessions", "users"
