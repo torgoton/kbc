ceec4f5f6096cf121360841445bea621ba6a9096
Chris <chris@idlelion.net>
2025-04-14 16:04:32 -0500

    Updates to player area

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @my_games = Current.user.games
-    @other_games = (GamePlayer.joins(:game).
-      where("game_players.user_id != ?", Current.user.id).
-      map(&:game).
-      uniq.
-      sort_by(&:id)) - @my_games
+    @open_games = Game.joins(:game_players).where(state: "waiting").where.not(game_players: { user_id: Current.user.id }).sort_by(&:id)
   end
 end
