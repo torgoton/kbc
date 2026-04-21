ff69f8f23dc7d520847cf48008db7ac70d4df588
Chris Schumann <chris@idlelion.net>
2026-04-21 16:55:59 -0500

    Different lighthouse/boat icon

--- app/models/tiles/lighthouse_tile.rb
+++ app/models/tiles/lighthouse_tile.rb
@@ -1,6 +1,6 @@
 module Tiles
   class LighthouseTile < Tiles::Tile
-    CREATOR = "Icon by Risa Yustissa".freeze
+    CREATOR = "Icon by franc11s".freeze
     DESCRIPTION = "Place, move, or remove your ship".freeze
   end
 end
