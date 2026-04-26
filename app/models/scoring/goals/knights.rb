68219e0a9fd268c07638f5d876836166ec70608b
Chris Schumann <chris@idlelion.net>
2026-04-26 15:20:38 -0500

    Better knights language

--- app/models/scoring/goals/knights.rb
+++ app/models/scoring/goals/knights.rb
@@ -1,7 +1,7 @@
 class Scoring
   module Goals
     class Knights < Goal
-      DESCRIPTION = "2 points for each settlement on the line with the most settlements"
+      DESCRIPTION = "2 points for each settlement on the horizontal line with the most of your settlements"
       def score_for(game_player)
         rows = settlements_for(game_player.order).map(&:first)
         best = rows.tally.values.max || 0
