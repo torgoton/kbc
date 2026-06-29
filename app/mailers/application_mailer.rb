709e90456080042c0b667c9cdd8716ec461a181d
Chris Schumann <chris@idlelion.net>
2026-06-29 18:11:36 -0500

    Also configure outgoing SMTP/mail API creds

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("MAIL_FROM", "kbc@mail.chrisschumann.dev")
+  default from: ENV.fetch("SITE_EMAIL") # Raises if not set
   layout "mailer"
 end
