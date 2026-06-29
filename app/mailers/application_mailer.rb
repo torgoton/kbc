2b29359c7c20794d518652d15506bb6595676fa8
Chris Schumann <chris@idlelion.net>
2026-06-29 18:37:51 -0500

    More email futzing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("MAIL_FROM") # Raises if not set
+  default from: ENV.fetch("MAIL_FROM"), # Raises if not set,
+          reply_to: ENV.fetch("REPLY_TO", ENV.fetch("MAIL_FROM"))
   layout "mailer"
 end
