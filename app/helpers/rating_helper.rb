c35b472a8102d85c89e6f2c72441221ffc1d2582
Chris Schumann <chris@idlelion.net>
2026-07-01 12:53:59 -0500

    Tweaks for rating displays

--- app/helpers/rating_helper.rb
+++ app/helpers/rating_helper.rb
@@ -3,4 +3,8 @@ module RatingHelper
     provisional = user.rated_games_count < Rating::CONFIG[:provisional_games]
     "#{user.rating}#{"?" if provisional}"
   end
+
+  def rated_list(game)
+    game.players.map { |player| "#{player.handle} (#{rating_badge(player)})" }.join(", ")
+  end
 end
